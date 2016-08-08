5ab17cfeb3f87029718c56f4dfc71e174f9390e4
Roger Goodway <goodwayroger@gmail.com>
2016-08-08 06:51:07 +0300

    'ajax_valid_upload_img'

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,23 +5,31 @@
     <meta name="csrf-token" content="{!! csrf_token() !!}" />
     <title>Document</title>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    <script src="js/lib/jquery-1.11.1.js"></script>
+    <script src="js/lib/jquery.js"></script>
+    <script src="js/dist/jquery.validate.js"></script>
+    <script src="js/dist/additional-methods.js"></script>
 </head>
 <body>
     <div class="container">
         <a href="#" id="get">Get</a>
         <hr>
 
-        <form action="#" enctype="multipart/form-data">
-            <label for="">name</label>
-            <input type="text" name="name">
-            <br>
-            <br>
-            <input type="file" id="images" name="images[]" multiple="multiple">
-            <br>
-            <br>
-            <input type="hidden" name="_token" value="{!! csrf_token() !!}">
-            <button type="submit">Submit</button>
-        </form>
+        {!! Form::open(array('url' => '#', 'method' => 'post', 'files' => true, 'id' => 'fileForm', 'class'=>'img-form')) !!}
+            <p>
+                <label for="">name</label>
+                <input type="text" name="name">
+            </p>
+            <p>
+                <label for="file4">Select any audio or image file</label>
+                <input type="file" id="file4" class="required" name="file4[]" multiple="multiple" accept="image/*,audio/*">
+            </p>
+            <p>
+                <input type="hidden" name="_token" value="{!! csrf_token() !!}">
+                <button type="submit">Submit</button>
+            </p>
+
+        {!! Form::close() !!}
     </div>
 
     <script>
@@ -33,6 +41,8 @@
         });
 
         $(document).ready(function() {
+
+            $("#fileForm").validate();
 
             $('#get').click(function(e) {
                 e.preventDefault();
@@ -47,7 +57,7 @@
                 e.preventDefault();
 
                 var data = new FormData();
-                $.each($('#images')[0].files, function(i, file) {
+                $.each($('#file4')[0].files, function(i, file) {
                     data.append('file-'+i, file);
                 });
 
